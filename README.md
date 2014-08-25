b439917d7a4518e9db262032b1f4c8e1b97314df
guodongxiaren <879231132@qq.com>
2014-08-25 09:49:34 +0800

    up stat

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ my_linux_cmd
 ============
 自己动手实现Linux命令
 ###stat~
+##官方源码
+[coreuils](http://ftp.gnu.org/gnu/coreutils/)
+[bash](http://ftp.gnu.org/gnu/bash/)
--- stat.c
+++ stat.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -9,6 +10,13 @@ int main(int argc,char *argv[])
     printf("  文件：\"%s\"\n",argv[1]);
     printf("  大小：%ld\t\t块: %ld\t\tIO 块：%ld\n",buf.st_size,
          buf.st_blocks,buf.st_blksize);
-    printf("设备：%u%02u/h%ldd\n",major(buf.st_dev),
+    printf("设备：%u%02u/h%ldd\t",major(buf.st_dev),
            minor(buf.st_dev),buf.st_dev);
+    printf("Inode：%ld\t硬链接：%ld\n",buf.st_ino,buf.st_nlink);
+
+    printf("权限：%u\n",buf.st_mode);
+    printf("Uid：%d\tGid：%d\n",buf.st_uid,buf.st_gid);
+    printf("最近访问：%s",ctime(&buf.st_atime));
+    printf("最近更改：%s",ctime(&buf.st_mtime));
+    printf("最近改动：%s",ctime(&buf.st_ctime));
 }
