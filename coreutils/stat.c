95044ff6437aedca66799a0bd14b2ef497597acc
guodongxiaren <879231132@qq.com>
2014-10-13 22:31:18 +0800

    简化stat.c的代码 原来在进制转换的时候，我选择八进制转换成十进制。实际上并没有这个必要。直接按八进制处理就行了。

--- coreutils/stat.c
+++ coreutils/stat.c
@@ -5,10 +5,92 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-char *file_type(mode_t st_mode,char *type);
-void permission(mode_t st_mode,char *per,int *code);
-int o2d(int mode);
-
+char *file_type(mode_t st_mode,char *type)
+{
+    char *ptr;
+    if (S_ISREG(st_mode))
+    {
+        ptr = "regular";
+        *type = '-';
+    }
+    else if (S_ISDIR(st_mode))
+    {
+        ptr = "directory";
+        *type = 'd';
+    }
+    else if (S_ISCHR(st_mode))
+    {
+        ptr = "character special";
+        *type = 'c';
+    }
+    else if (S_ISBLK(st_mode))
+    {
+        ptr = "block special";
+        *type = 'b';
+    }
+    else if (S_ISFIFO(st_mode))
+    {
+        ptr = "fifo";
+        *type = 'p';
+    }
+    else if (S_ISLNK(st_mode))
+    {
+        ptr = "symbolic link";
+        *type = 'l';
+    }
+    else if (S_ISSOCK(st_mode))
+    {
+        ptr = "socket";
+        *type = 's';
+    }
+    else
+        ptr = "unknown mode";
+    return ptr;
+}
+void permission(mode_t st_mode,char *per)
+{
+    int len=9;
+    int bit;
+    while(len--)
+    {
+        bit=st_mode&1;
+        st_mode>>=1;
+        if(!bit)
+            continue;
+        switch(len%3)
+        {
+        case 2:
+            per[len]='x';
+            break;
+        case 1:
+            per[len]='w';
+            break;
+        case 0:
+            per[len]='r';
+            break;
+        }
+    }
+    len=3;
+    while(len--)
+    {
+        bit=st_mode&1;
+        st_mode>>=1;
+        if(!bit)
+            continue;
+        switch(len%3)
+        {
+        case 2:
+            per[8]='t';
+            break;
+        case 1:
+            per[5]='s';
+            break;
+        case 0:
+            per[2]='s';
+            break;
+        }
+    }
+}
 int main(int argc,char *argv[])
 {
     struct stat buf;
@@ -23,9 +105,8 @@ int main(int argc,char *argv[])
     printf("Inode：%ld\t硬链接：%ld\n",buf.st_ino,buf.st_nlink);
 
     char per[]="---------";
-    int code;
-    permission(buf.st_mode,per,&code);
-    printf("权限：(%04d/%c%s)  ",code,type,per);
+    permission(buf.st_mode,per);
+    printf("权限：(%04o/%c%s)  ",buf.st_mode%010000,type,per);
 
     struct passwd *passwd;
     struct group *group;
@@ -36,91 +117,4 @@ int main(int argc,char *argv[])
     printf("最近访问：%s",ctime(&buf.st_atime));
     printf("最近更改：%s",ctime(&buf.st_mtime));
     printf("最近改动：%s",ctime(&buf.st_ctime));
-}
-char *file_type(mode_t st_mode,char *type)
-{
-    char *ptr;
-    if (S_ISREG(st_mode)) 
-    {
-        ptr = "regular"; 
-        *type = '-';
-    }
-    else if (S_ISDIR(st_mode)) 
-    {
-        ptr = "directory"; 
-        *type = 'd';
-    }
-    else if (S_ISCHR(st_mode)) 
-    {
-        ptr = "character special"; 
-        *type = 'c';
-    }
-    else if (S_ISBLK(st_mode)) 
-    {
-        ptr = "block special"; 
-        *type = 'b';
-    }
-    else if (S_ISFIFO(st_mode)) 
-    {
-        ptr = "fifo"; 
-        *type = 'p';
-    }
-    else if (S_ISLNK(st_mode)) 
-    {
-        ptr = "symbolic link"; 
-        *type = 'l';
-    }
-    else if (S_ISSOCK(st_mode)) 
-    {
-        ptr = "socket"; 
-        *type = 's';
-    }
-    else 
-    ptr = "unknown mode"; 
-    return ptr;
-}
-void permission(mode_t st_mode,char *per,int *code)
-{
-    *code=o2d(st_mode)%10000;
-    int len=9;
-    int bit;
-    while(len--)
-    {
-        bit=st_mode&1;
-        st_mode>>=1;
-        if(!bit)
-        continue;
-        switch(len%3)
-        {
-            case 2:per[len]='x';break;
-            case 1:per[len]='w';break;
-            case 0:per[len]='r';break;
-        }
-    }
-    len=3;
-    while(len--)
-    {
-        bit=st_mode&1;
-        st_mode>>=1;
-        if(!bit)
-        continue;
-        switch(len%3)
-        {
-            case 2:per[8]='t';break;
-            case 1:per[5]='s';break;
-            case 0:per[2]='s';break;
-        }
-    }
-}
-int o2d(int mode)
-{
-    int res=0;
-    int base=1;
-    while(mode)
-    {
-        res+=mode%8*base;
-        base*=10;
-        mode/=8;
-    }
-    return res;
 }
